782d06115357868202e1e5f061cfb88189682e69
Mauricio de Oliveira <maufrco@gmail.com>
2022-03-28 00:08:16 -0300

    Add endpoint

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const express = require ('express')
 const cors = require('cors'); 
 const path  = require ('path') 
 const app = express () 
-const apiRouter = require('./api/routes/api_routes') 
+const apiRouter = require('./api/routes/apiRouter') 
 
 
 app.use(cors()) 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
  
+app.use('/', express.static(path.join(__dirname, '/public'))) 
 app.use('/app', express.static(path.join(__dirname, '/public'))) 
 app.use ('/api', apiRouter) 
  
  
 let port = process.env.PORT || 3000 
-app.listen (port) 
+app.listen(port) 
  
