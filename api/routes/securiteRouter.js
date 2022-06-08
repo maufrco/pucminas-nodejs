3835364a3f89e6ac4657e2a65909e4517f544f5c
Mauricio de Oliveira <maufrco@gmail.com>
2022-06-08 01:57:38 -0300

    Fix catch

--- api/routes/securiteRouter.js
+++ api/routes/securiteRouter.js
@@ -35,8 +35,7 @@ securiteRouter.post (endpoint + 'register', (req, res) => {
             return 
         }) 
         .catch(err => { 
-            res.status(500).json({  
-                message: 'Erro ao registrar usuario - ' + err.message }) 
+            res.status(500).json(err) 
         })   
   }) 
   
@@ -68,7 +67,7 @@ securiteRouter.post (endpoint + 'register', (req, res) => {
       }) 
       .catch (err => { 
           res.status(500).json({  
-             message: 'Erro ao verificar login - ' + err.message }) 
+             message:  err.message }) 
       }) 
   }) 
 
